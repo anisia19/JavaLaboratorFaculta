9df597880a03c98cf91f2f913e2931ffbe544181
anisia19 <anisia.fort19@gmail.com>
2025-10-07 14:22:31 +0300

    Ex 1 pb cu judete

---
+++ Laborator2/Lab2ExRezolvatePractice/src/exercitiul1/MainApp.java
@@ -0,0 +1,41 @@
+package exercitiul1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args) {
+        String[] judete = new String[0];
+        String date_in = "C:\\Users\\gheor\\Desktop\\JavaLaboratorFaculta\\Laborator2\\Lab2ExRezolvatePractice\\src\\exercitiul1\\judete_in.txt";
+        try {
+            BufferedReader flux_in = new BufferedReader(new FileReader(date_in));
+            StringBuilder continut = new StringBuilder();
+            String linie;
+            while((linie = flux_in.readLine()) != null){
+                continut.append(linie);
+            }
+            judete = continut.toString().split(",");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < judete.length; i++) {
+            judete[i] = judete[i].trim();
+        }
+        Arrays.sort(judete);
+        System.out.println("Ordonarea alfabetica a judetelor este: ");
+        for(int i = 0; i < judete.length; i++){
+            System.out.println(" " + judete[i].trim());
+        }
+        System.out.print("Introdu un judet pentru a afla pozitia lui: ");
+        Scanner scanner = new Scanner(System.in);
+        String judet_citit = scanner.nextLine();
+        scanner.close();
+        int poz = Arrays.binarySearch(judete, judet_citit);
+        System.out.println(poz >= 0?"Gasit pe pozitia " + poz:"Nu se gaseste!");
+    }
+}
+
