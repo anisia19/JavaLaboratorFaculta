a7742d2db162944e002a4c1655b872df698ad775
anisia19 <anisia.fort19@gmail.com>
2025-10-07 15:48:47 +0300

    Ex2, rezolvare, pb cu melodia
    Folosirea metodelor din String.
    Lucru cu fisiere, siruri de caractere.

---
+++ Laborator2/Lab2ExRezolvatePractice/src/exercitiul2/Vers.java
@@ -0,0 +1,50 @@
+package exercitiul2;
+
+import java.util.Arrays;
+
+public class Vers {
+    private String vers;
+    public Vers(String vers) {
+        this.vers = vers;
+    }
+    public Vers() {
+    }
+
+    public String getVers() {
+        return vers;
+    }
+    public void setVers(String vers){
+        this.vers = vers;
+    }
+
+    public int nrCuvinte() {
+        if (vers == null || vers.trim().isEmpty()) {
+            return 0;
+        }
+        String[] cuvinte = vers.trim().split("\\s+");
+        return cuvinte.length;
+    }
+
+    public int cautaVocale(){
+        String vocale = "aeiouAEIOU";
+        int nr_vocale = 0;
+        for (char c : vocale.toCharArray()){
+            if(vers.contains(String.valueOf(c))){
+                nr_vocale++;
+            }
+        }
+        return nr_vocale;
+    }
+    public boolean addStelutza(){
+       if(vers.endsWith("say?"))
+                return true;
+        return false;
+    }
+    public void randomNumber(){
+        double randomNum = Math.random(); // generează un număr între 0.0 și 1.0
+        if (randomNum < 0.1) {
+            vers = vers.toUpperCase(); // face tot stringul uppercase
+        }
+    }
+}
+
